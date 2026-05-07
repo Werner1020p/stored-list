1c93203b08203cfa093214cb1f5034b7816e3598
Dora Do <doradocodes@gmail.com>
2026-05-07 01:24:16 -0400

    init

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const Items = require('../models/items');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    // Your code goes here
+
+});
+
+module.exports = router;
